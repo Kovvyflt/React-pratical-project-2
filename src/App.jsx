d93d8cc69a1f60acd42b70a432680ccbae1b50c3
Kovvyflt <Covshow5@gmail.com>
2023-11-03 12:26:44 +0100

    Second react pratical project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, Fragment } from 'react';
+
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Header from './components/Header/Header.jsx';
+import Examples from './components/Header/Examples.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts/>
+        <Examples />
+      </main>
+    </>
+  );
+}
+
+export default App;
